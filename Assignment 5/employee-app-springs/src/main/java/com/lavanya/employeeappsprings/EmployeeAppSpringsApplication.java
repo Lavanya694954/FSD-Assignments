a6f571f648cbf2b8f713a5e952dc0cc7482d2bb1
Lavanya694 <64001062+Lavanya694@users.noreply.github.com>
2021-05-11 16:16:18 +0530

    added assignment 5

---
+++ Assignment 5/employee-app-springs/src/main/java/com/lavanya/employeeappsprings/EmployeeAppSpringsApplication.java
@@ -0,0 +1,13 @@
+package com.lavanya.employeeappsprings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAppSpringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAppSpringsApplication.class, args);
+	}
+
+}
